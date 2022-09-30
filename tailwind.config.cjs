3ee5106bcf442ab365a54f46a759986385a40159
viniciusventura29 <viniciusventura29@gmail.com>
2022-09-30 10:57:56 -0300

    improving css

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors :{
+				primaryBlue: "#08678C",
+				secondaryBlue:'#074973',
+				tertiaryBlue:'#032859',
+				quaternaryBlue:'#011C40',
+				darkBlue:"#011126",
+			}
+		},
 	},
 	plugins: [],
 }
