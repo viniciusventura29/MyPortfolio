dd9990f4955a62d0b4fb49967843a6162bf6a774
viniciusventura29 <viniciusventura29@gmail.com>
2023-12-06 15:59:07 -0300

    hiding drafts

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,22 +1,25 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { TITLE, DESCRIPTION } from '../sitesInfo';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { TITLE, DESCRIPTION } from "../sitesInfo";
 
 export async function get(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: TITLE,
-		description: DESCRIPTION,
-		site: context.site,
-		items: posts.filter(post=>post.data.title !== "Vinicius Ventura!").map((post) => ({
-			...post.data,
-            pubDate:post.data.publishDate,
-			enclosure: {
-				length: 1,
-				type: "img",
-				url: post.data.image
-			},
-			link: `/blog/${post.slug}/`,
-		})),
-	});
+  const posts = await getCollection("blog");
+  return rss({
+    title: TITLE,
+    description: DESCRIPTION,
+    site: context.site,
+    items: posts
+      .filter((post) => post.data.title !== "Vinicius Ventura!")
+      .filter((post) => post.data.draft !== true)
+      .map((post) => ({
+        ...post.data,
+        pubDate: post.data.publishDate,
+        enclosure: {
+          length: 1,
+          type: "img",
+          url: post.data.image,
+        },
+        link: `/blog/${post.slug}/`,
+      })),
+  });
 }
