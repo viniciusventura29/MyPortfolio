457193da3cd8848a78de21b29e001fbd37bee4e5
viniciusventura29 <viniciusventura29@gmail.com>
2023-12-06 09:00:18 -0300

    adding rss

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,21 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import { TITLE, DESCRIPTION } from '../sitesInfo';
+
+export async function get(context) {
+	const posts = await getCollection('blog');
+	return rss({
+		title: TITLE,
+		description: DESCRIPTION,
+		site: context.site,
+		items: posts.map((post) => ({
+			...post.data,
+			enclosure: {
+				length: 1,
+				type: "img",
+				url: post.data.image
+			},
+			link: `/blog/${post.slug}/`,
+		})),
+	});
+}
---
+++ src/sitesInfo.ts
@@ -0,0 +1,2 @@
+export const TITLE = 'Vinicius Ventura';
+export const DESCRIPTION = "This is my website, that contain my blog, a little bit about me and also some projects that I developed.";
