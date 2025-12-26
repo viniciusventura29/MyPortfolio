f066897662cc56c4b684d6385f6102c7e9854076
viniciusventura29 <viniciusventura29@gmail.com>
2025-12-26 01:13:46 -0300

    Refactor blog content: migrate monovsmicro.md to monovsmicro.mdx for enhanced structure and component integration, and remove outdated noName.md to streamline content organization.

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,4 +1,5 @@
 
 export default new Map([
-["src/content/blog/contextIsKing.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2FcontextIsKing.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/contextIsKing.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2FcontextIsKing.mdx&astroContentModuleFlag=true")],
+["src/content/blog/monovsmicro.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmonovsmicro.mdx&astroContentModuleFlag=true")]]);
 		
