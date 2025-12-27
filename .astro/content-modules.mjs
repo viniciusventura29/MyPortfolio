0a56f32971ff66dc3425b4f279fc0a5b03df3f85
viniciusventura29 <viniciusventura29@gmail.com>
2025-12-27 04:10:26 -0300

    Add TrampoAI blog post with MCP Mesh explanation and demo video

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,5 +1,6 @@
 
 export default new Map([
-["src/content/blog/contextIsKing.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2FcontextIsKing.mdx&astroContentModuleFlag=true")],
-["src/content/blog/monovsmicro.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmonovsmicro.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/monovsmicro.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmonovsmicro.mdx&astroContentModuleFlag=true")],
+["src/content/blog/trampoAi.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2FtrampoAi.mdx&astroContentModuleFlag=true")],
+["src/content/blog/contextIsKing.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2FcontextIsKing.mdx&astroContentModuleFlag=true")]]);
 		
