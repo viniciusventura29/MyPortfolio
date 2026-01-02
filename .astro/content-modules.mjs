42e0f047ca24e8e2f9955b543f903c0b95edb070
viniciusventura29 <viniciusventura29@gmail.com>
2026-01-02 03:22:50 -0300

    Add new blog post "Purpose" with insights on AI context and MCP integration. Update content-modules to include new blog references and enhance layout styles for improved visual consistency.

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,6 +1,8 @@
 
 export default new Map([
+["src/content/blog/contextIsKing.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2FcontextIsKing.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/monovsmicro.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmonovsmicro.mdx&astroContentModuleFlag=true")],
-["src/content/blog/trampoAi.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2FtrampoAi.mdx&astroContentModuleFlag=true")],
-["src/content/blog/contextIsKing.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2FcontextIsKing.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/contextIsKing copy.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2FcontextIsKing+copy.mdx&astroContentModuleFlag=true")],
+["src/content/blog/porpuse.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fporpuse.mdx&astroContentModuleFlag=true")],
+["src/content/blog/purpose.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fpurpose.mdx&astroContentModuleFlag=true")]]);
 		
